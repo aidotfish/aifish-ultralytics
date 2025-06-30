5a0e645b812e06f01e75f5012bdbd440e426f453
Til-Widmann <64083830+Til-Widmann@users.noreply.github.com>
2025-06-30 12:25:00 +0100

    intermediate save

--- experiments/ignore_zones/run_simple_train.py
+++ experiments/ignore_zones/run_simple_train.py
@@ -5,7 +5,7 @@ model = YOLO(model='yolov8n.pt', task='detect')
 
 model.train(
     data='/Users/tilwidmann/Work/AiFish/datasets/ignore_zones/dataset.yaml',
-    epochs=2,
-    ignore_zone_cls=[0],
+    epochs=100,
+    ignore_zone_cls=1,
     device='mps',
 )
